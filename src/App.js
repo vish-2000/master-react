b372edc33593988ce950a5fa6af3b00e30825a91
Vishesh Agarwal <vishesh.agarwal201215@gmail.com>
2023-02-09 02:18:13 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
-import List1, {List2} from "./DataMap/List1";
-function App(){
-    const NameList = ['Anjani','Vishesh','Joyeta','Sarani']
 
+import TodoComponent from "./Todo/TodoComponent";
+import Blog from "./Blog/Blog";
+
+import BasicLifeCycleCompnent from "./ClassComponentAll/LifeCycle/BasicLifeCycleComponent";
+import AdvanceLifeCycleComponent from "./ClassComponentAll/LifeCycle/AdvancedLifeCycleComponent";
+import LifeCycleConstructor from "./ClassComponentAll/Constructor/LifeCycleConstructor";
+import LifeCycleDerivedState from "./ClassComponentAll/getDerivedStateFromProps/LifeCycleDerivedState";
+import LYComponentDidMount from "./ClassComponentAll/componentDidMount/LYComponentDidMount";
+import LyShouldComponentUpdate from "./ClassComponentAll/ShouldComponentUpdate/LyShouldComponentUpdate";
+import LyComponentDidUpdate from "./ClassComponentAll/componentDidUpdate/LYComponentDidUpdate";
+import LyDidCatch from "./ClassComponentAll/DidCatch/LyDidCatch";
+
+function App(){
     return(
-        <>
-            <List1 />
-            <List2 list={NameList}/>
-        </>
+        //<TodoComponent/>
+        //<Blog />
+
+        //<BasicLifeCycleCompnent/>
+        //<AdvanceLifeCycleComponent/>
+
+        //<LifeCycleConstructor/>
+        //<LifeCycleDerivedState col='yellow'/>
+
+        //<LYComponentDidMount/>
+        //<LyShouldComponentUpdate/>
+        //<LyComponentDidUpdate/>
+
+        <LyDidCatch/>
     );
 }
 
